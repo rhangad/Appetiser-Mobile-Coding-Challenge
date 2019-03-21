effda89342c9abbddd793c1fec69537759c7f41a
Dev Partners <devpartners@Devs-MacBook-Pro.local>
2019-03-21 08:58:51 +0800

    Initial Commit

---
+++ Appetiser Mobile Coding Challenge/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Appetiser Mobile Coding Challenge
+//
+//  Created by Dev Partners on 3/21/19.
+//  Copyright © 2019 Dev Partners. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ Appetiser Mobile Coding ChallengeTests/Appetiser_Mobile_Coding_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Appetiser_Mobile_Coding_ChallengeTests.swift
+//  Appetiser Mobile Coding ChallengeTests
+//
+//  Created by Dev Partners on 3/21/19.
+//  Copyright © 2019 Dev Partners. All rights reserved.
+//
+
+import XCTest
+@testable import Appetiser_Mobile_Coding_Challenge
+
+class Appetiser_Mobile_Coding_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
